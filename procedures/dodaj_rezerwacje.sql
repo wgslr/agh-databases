2ed090a9dfeacf404a1d34be174bd83bc14e066f
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-10-22 21:09:18 +0200

    Add dodaj_rezerwacje procedure with incomplete error checks

---
+++ procedures/dodaj_rezerwacje.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE PROCEDURE
+  dodaj_rezerwacje(id_wycieczki WYCIECZKI.ID_WYCIECZKI%TYPE, id_osoby OSOBY.ID_OSOBY%TYPE) AS
+  istnieje integer;
+  BEGIN
+    SELECT COUNT(*) INTO istnieje FROM OSOBY WHERE OSOBY.ID_OSOBY = dodaj_rezerwacje.id_osoby;
+
+    IF istnieje = 0
+    THEN
+      raise_application_error(-20002, 'Brak osoby o podanym id');
+    END IF;
+
+    SELECT COUNT(*) INTO istnieje FROM DOSTEPNE_WYCIECZKI_VIEW w WHERE w.ID_WYCIECZKI = dodaj_rezerwacje.id_wycieczki;
+
+    IF istnieje = 0
+    THEN
+      raise_application_error(-20004, 'Brak dostępnej wycieczki o podanym id');
+    END IF;
+
+    -- @fixme check if full
+    INSERT INTO REZERWACJE (id_wycieczki, id_osoby, STATUS)
+    VALUES (dodaj_rezerwacje.id_wycieczki, dodaj_rezerwacje.id_osoby, 'N');
+
+  END dodaj_rezerwacje;
+
+  BEGIN
+    dodaj_rezerwacje(4, 6);
+  end;
+
+    SELECT * FROM DOSTEPNE_WYCIECZKI_VIEW
--- views/dostepne_wycieczki_view.sql
+++ views/dostepne_wycieczki_view.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW dostepne_wycieczki_view
   AS
-    SELECT w.kraj, w.data, w.nazwa, w.liczba_miejsc, w.liczba_wolnych_miejsc
+    SELECT w.id_wycieczki, w.kraj, w.data, w.nazwa, w.liczba_miejsc, w.liczba_wolnych_miejsc
     FROM wycieczki_miejsca w
     WHERE liczba_wolnych_miejsc > 0
       AND w.DATA > CURRENT_DATE;
