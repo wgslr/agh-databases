efe9ce7213e302703c6b255fe4dce8e6349b09a4
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-10-22 18:15:54 +0200

    Worked at home, fails at work

--- procedures/uczestnicy_wycieczki.sql
+++ procedures/uczestnicy_wycieczki.sql
@@ -1,36 +1,35 @@
-CREATE OR REPLACE TYPE osoby_record AS object (
-  nazwa VARCHAR(100),
-  kraj  VARCHAR(50)
+CREATE OR REPLACE TYPE osoby_wycieczki_r AS object (
+  kraj            VARCHAR(50),
+  "data"          DATE,
+  nazwa_wycieczki VARCHAR(100),
+  imie            VARCHAR2(50),
+  nazwisko        VARCHAR2(50),
+  status          CHAR(1)
 );
 
-CREATE OR REPLACE TYPE osoby_table as table OF osoby_record;
+
+CREATE OR REPLACE TYPE osoby_wycieczki_t IS TABLE OF osoby_wycieczki_r;
+
+-- @fixme validate wycieczka exists
 
 CREATE OR REPLACE
-FUNCTION uczestnicy_wycieczki (id INT)
-  return osoby_table as
-  v_ret osoby_table;
-  BEGIN
-    SELECT osoby_record(W.NAZWA, W.KRAJ)
-    BULK COLLECT INTO v_ret
-    FROM WYCIECZKI W
-    WHERE W.ID_WYCIECZKI = uczestnicy_wycieczki.id;
+FUNCTION uczestnicy_wycieczki(id INT)
+  return osoby_wycieczki_t as v_ret osoby_wycieczki_t;
 
---     v_ret := osoby_table();
---     v_ret.extend; v_ret(v_ret.count) := osoby_record('Makabra2', 'Transylwania');
---     v_ret := (SELECT --w.ID_WYCIECZKI,
---                    w.NAZWA, w.KRAJ --,
---         --                    w.DATA,
---         --                    o.IMIE,
---         --                    o.NAZWISKO,
---         --                    r.STATUS
---             FROM WYCIECZKI w
---                    JOIN REZERWACJE r ON w.ID_WYCIECZKI = r.ID_WYCIECZKI
---                    JOIN OSOBY o ON r.ID_OSOBY = o.ID_OSOBY
---             WHERE w.ID_WYCIECZKI = id);
+  BEGIN
+    SELECT
+    BULK COLLECT INTO osoby_wycieczki_r(w.KRAJ, w.DATA, w.NAZWA, o.IMIE,
+                                               o.NAZWISKO, r.STATUS)
+    FROM WYCIECZKI w
+           JOIN REZERWACJE r ON w.ID_WYCIECZKI = r.ID_WYCIECZKI
+           JOIN OSOBY o ON r.ID_OSOBY = o.ID_OSOBY;
     return v_ret;
   end uczestnicy_wycieczki;
 
-  SELECT * FROM uczestnicy_wycieczki(1)
+
+SELECT *
+FROM uczestnicy_wycieczki(3);
 
 
-SELECT * FROM WYCIECZKI
+SELECT *
+FROM WYCIECZKI
