f356f3e880de7d09f48b2e4ee35be9bbd578bac3
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-10-24 02:16:45 +0200

    Restore seed value

--- seed.sql
+++ seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO osoby (imie, nazwisko, pesel, kontakt)
 VALUES ('Adam', 'Kowalski', '87654321', 'tel: 6623');
 INSERT INTO osoby (imie, nazwisko, pesel, kontakt)
-VALUES ('Jan', 'Nowak', '12345678', 'tel: 2312, tylko po 18.00');
+VALUES ('Jan', 'Nowak', '12345678', 'tel: 2312, dzwonić po 18.00');
 INSERT INTO osoby (imie, nazwisko, pesel, kontakt)
 VALUES ('Janek', 'Nowaczek', '87654332', 'fax: 2312');
 INSERT INTO osoby (imie, nazwisko, pesel, kontakt)
