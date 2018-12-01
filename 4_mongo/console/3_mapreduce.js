390fb08ffc3b80b49532c8cb806dad9b6cb33071
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-02 00:38:26 +0100

    Use correct map and reduce functions with same outputs

--- 4_mongo/console/3_mapreduce.js
+++ 4_mongo/console/3_mapreduce.js
@@ -1,25 +1,31 @@
 db.question.mapReduce(
   function() {
-    emit(this.round, new Date(this.air_date))
+    const value = {
+      latest: new Date(this.air_date),
+      avg: 0, // average time difference
+      count: 0 // element in the average
+    }
+    emit(this.round, value)
   },
   function(key, value) {
-    const MILLIS_IN_A_DAY = 86400000;
-    let prevDate = null;
-    let differences = []
+    // sort dates in ascending order
+    value = value.sort((x, y) => x.latest - y.latest)
 
-    value = value.sort((x, y) => x - y)
+    let v = value[0]
 
-    for(let i = 0; i < value.length; ++i) {
-      if(prevDate != null){
-        diffDays = (value[i] - prevDate) / MILLIS_IN_A_DAY
-        differences.push(diffDays)
-      }
-      prevDate = value[i]
+    for(let i = 1; i < value.length; ++i) {
+      const newCount = v.count + 1;
+      v.avg = (v.avg * v.count + (value[i].latest - v.latest)) / newCount
+      v.latest = value[i].latest
+      v.count = newCount
     }
-    // average number of days between rounds appeared
-    return Array.sum(differences) / differences.length
+    return v;
   },
   {
-    out: "freq"
+    out: "freq",
+    finalize: function(key, reducedValue) {
+      const MILLIS_IN_A_DAY = 86400000;
+      return reducedValue.avg / MILLIS_IN_A_DAY
+    }
   }
 )
