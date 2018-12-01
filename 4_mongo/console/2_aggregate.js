86eeacd9612451a01b648e5399add3ee4f7f4cd2
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-02 00:11:24 +0100

    Write mapreduce for calculating timespans

--- 4_mongo/console/2_aggregate.js
+++ 4_mongo/console/2_aggregate.js
@@ -1,3 +1,4 @@
+use jeopardy
 db.question.aggregate([
         {$addFields:
           {
---
+++ 4_mongo/console/3_mapreduce.js
@@ -0,0 +1,25 @@
+db.question.mapReduce(
+  function() {
+    emit(this.round, new Date(this.air_date))
+  },
+  function(key, value) {
+    const MILLIS_IN_A_DAY = 86400000;
+    let prevDate = null;
+    let differences = []
+
+    value = value.sort()
+
+    for(let i = 0; i < value.length; ++i) {
+      if(prevDate != null){
+        diffDays = (value[i] - prevDate) / MILLIS_IN_A_DAY
+        differences.push(diffDays)
+      }
+      prevDate = value[i]
+    }
+    // average number of days between rounds appeared
+    return Array.sum(differences) / differences.length
+  },
+  {
+    out: "freq"
+  }
+)
