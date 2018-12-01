d067b3419e827e914ce672e1fe724a15b377ccf7
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-01 23:43:00 +0100

    Create query for counting shows during each year

--- 4_mongo/console/2_aggregate.js
+++ 4_mongo/console/2_aggregate.js
@@ -1,11 +1,17 @@
 db.question.aggregate([
         {$addFields:
-          {year: {$year: {$toDate: "$air_date"}}}
+          {
+            year: {$year: {$toDate: "$air_date"}},
+            show_number: {$toInt: "$show_number"}
+          }
         },
         {$group:
-          {_id: "$year", count: {$sum: 1}, first: {$min: "$show_number"}}
+          {_id: "$show_number", year: {$min: "$year"}}
+        },
+        {$group:
+          {_id: "$year", count: {$sum: 1}, first_show: {$min: "$_id"}}
         },
         {$sort:
-          {count: -1}
+          {count: -1, first: -1}
         }
 ])
