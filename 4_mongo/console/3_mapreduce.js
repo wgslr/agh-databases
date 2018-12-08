eca618f5949dc7ac28f28087fd87060ad7323d62
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-08 13:43:42 +0100

    Sort ranges before counting averages

--- 4_mongo/console/3_mapreduce.js
+++ 4_mongo/console/3_mapreduce.js
@@ -1,6 +1,8 @@
+db.question.createIndex({air_date: 1})
 db.question.mapReduce(
   function() {
     const value = {
+      earliest: new Date(this.air_date),
       latest: new Date(this.air_date),
       avg: 0, // average time difference
       count: 0 // element in the average
@@ -9,23 +11,29 @@ db.question.mapReduce(
   },
   function(key, value) {
     // sort dates in ascending order
-    value = value.sort((x, y) => x.latest - y.latest)
+    
+    // FIXME ensure that no overlapping ranges are being merged
+    value = value.sort((x, y) => x.latest - y.latest);
 
-    let v = value[0]
+    let v = value[0];
 
     for(let i = 1; i < value.length; ++i) {
-      const newCount = v.count + 1;
-      v.avg = (v.avg * v.count + (value[i].latest - v.latest)) / newCount
-      v.latest = value[i].latest
-      v.count = newCount
+      const curr = value[i];
+      const diff = curr.earliest - v.latest;
+      const newCount = v.count + curr.count + 1;
+
+      v.avg = (v.avg * v.count + curr.avg * curr.count + diff) / newCount;
+      v.latest = value[i].latest;
+      v.count = newCount;
     }
     return v;
   },
   {
     out: "freq",
+    sort: {air_date: 1},
     finalize: function(key, reducedValue) {
       const MILLIS_IN_A_DAY = 86400000;
-      return reducedValue.avg / MILLIS_IN_A_DAY
+      return reducedValue.avg / MILLIS_IN_A_DAY;
     }
   }
 )
